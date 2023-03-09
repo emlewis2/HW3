3e188cb99c8c295743639ce196e7f179924de8be
Libby Lewis <elewis43@jh.edu>
2023-03-08 21:43:38 -0600

    Removed bottom navigation

--- app/src/main/java/lewis/libby/hw3/components/ContactScaffold.kt
+++ app/src/main/java/lewis/libby/hw3/components/ContactScaffold.kt
@@ -1,15 +1,10 @@
 package lewis.libby.hw3.components
 
-import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.BottomNavigation
-import androidx.compose.material.BottomNavigationItem
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
@@ -19,10 +14,8 @@ import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-import lewis.libby.hw3.ContactList
 import lewis.libby.hw3.R
 import lewis.libby.hw3.Screen
 
@@ -132,39 +125,6 @@ fun ContactScaffold(
         },
         content = { paddingValues ->
             content(paddingValues)
-        },
-        bottomBar = {
-            BottomNavigation {
-                ScreenSelectButton(
-                    targetScreen = ContactList,
-                    imageVector = Icons.Default.Person,
-                    labelId = R.string.screen_title_contacts,
-                    onSelectListScreen = onSelectListScreen
-                )
-            }
         }
     )
 }
-
-@Composable
-fun RowScope.ScreenSelectButton(
-    targetScreen: Screen,
-    imageVector: ImageVector,
-    @StringRes labelId: Int,
-    onSelectListScreen: (Screen) -> Unit,
-) =
-    BottomNavigationItem(
-        selected = false,
-        icon = {
-            Icon(
-                imageVector = imageVector,
-                contentDescription = stringResource(id = labelId)
-            )
-        },
-        label = {
-            Text(text = stringResource(id = labelId))
-        },
-        onClick = {
-            onSelectListScreen(targetScreen)
-        }
-    )
